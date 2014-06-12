72d2a7c0d194d3e8b3b37b09dd716dd31d5965f2
boisde <boisde1117@gmail.com>
2014-06-12 18:11:35 +0800

    gnupg encryption @note virtual machine key generation extremely SLOW if not adding system proc entropy.

--- benchmark.py
+++ benchmark.py
@@ -27,13 +27,51 @@ class ParticleSimulator:
                 # 3. repeat for all the time_step
 
 
+from matplotlib import pyplot as plt
+from matplotlib import animation
+
+def visualize(simulator):
+    xs = [p.x for p in simulator.particles]
+    ys = [p.y for p in simulator.particles]
+    
+    fig = plt.figure()
+    ax = plt.subplot(111, aspect='equal')
+    line, = ax.plot(xs, ys, 'ro')
+# Axis limits
+    plt.xlim(-1, 1)
+    plt.ylim(-1, 1)
+# It will be run when the animation starts
+    def init():
+        line.set_data([],[])
+        return line,
+
+    def animate(i):
+        simulator.evolve(0.01)
+        x = [p.x for p in simulator.particles]
+        y = [p.y for p in simulator.particles]
+        line.set_data(x, y)
+        return line,
+
+    anim = animation.FuncAnimation(fig, animate, init_func=init, blit=True, interval=10)
+    plt.show()
+
 
 def test_visualize():
     particles = [Particle(0.1, 0.5, +1),
             Particle(0.0, -0.5, -1),
             Particle(-0.1, -0.4, +3)]
+    simulator = ParticleSimulator(particles)
+    visualize(simulator)
     print('hallo alice')
 
 
+from random import uniform
+
+def benchmark():
+    particles = [Particle(uniform(-1.0, +1.0),uniform(-1.0, +1.0),uniform(-1.0,+1.0)) for i in range(1000)]
+    simulator = ParticleSimulator(particles)
+    simulator.evolve(0.1)
+
+
 if __name__ == '__main__':
-    test_visualize()
+    benchmark()
--- gpgEncrypt.py
+++ gpgEncrypt.py
@@ -1,13 +1,37 @@
 import gnupg
 
 def test():
-    gpg = gnupg.GPG(gnupghome='/home/alice/Developer/Greed_Island/')
+    #key generation
+    gpg = gnupg.GPG(gnupghome='./.gnupg'#, verbose=Truei
+            )
     gpg.encoding = 'utf-8'
+    key = gpg.gen_key(gpg.gen_key_input())
+    fp = key.fingerprint
+    print 'key=[%s], fingerprint=[%s]'% (key, fp)
+    gpg.list_keys(True)
 
-    input_data = gpg.gen_key_input(key_type="RSA", key_length=1024)
-    key = gpg.gen_key(input_data)
-    print key
+#    data = raw_input("Enter full path of file to encrypt:")
+#    rkeys = raw_input("Enter key IDs seperated by spaces:")
+#    savefile = data+".asc"
+#    afile = open(data, "rb")
+#    encrypted_ascii_data = gpg.encrypt_file(afile, rkeys.split(), always_trust=True, output=savefile)
+#    afile.close()
+#    input_data = gpg.gen_key_input(key_type="RSA", key_length=1024)
+#    key = gpg.gen_key(input_data)
 
+    #encryption
+    with open('./README.md', 'rb') as content_file:
+        data = content_file.read()
+        print 'raw=[%s]'%data
+        encrypted_ascii_data = gpg.encrypt(str(data),fp)
+        print 'encrypted=[%s]'%encrypted_ascii_data
+        #decryption
+        decrypted_data = gpg.decrypt(str(encrypted_ascii_data))
+        print 'decrypted_data=[%s]'%decrypted_data
+
+    #delete key
+    print str(gpg.delete_keys(fp, True))
+    print str(gpg.delete_keys(fp))
 
 if __name__ == '__main__':
     test()
