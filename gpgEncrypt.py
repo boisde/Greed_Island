06c4fec8329b290435b37bc47d4be3e7cc93d2c1
boisde <boisde1117@gmail.com>
2014-06-30 19:27:38 +0800

    use only one key, import key if existed

--- gpgEncrypt.py
+++ gpgEncrypt.py
@@ -1,46 +1,55 @@
 import gnupg
 import os.path
-import shutil
 
 class GpgEncrypt:
     def __init__(self):
+        self.encrypted_file = ''
+        self.try_decoded_file = ''
+        self.exported_private_key_file = os.path.dirname(os.path.abspath(__file__))+'/exported_key.private'
+        self.exported_public_key_file = os.path.dirname(os.path.abspath(__file__))+'/exported_key.public'
         gnupg_path = os.path.dirname(os.path.abspath(__file__))+'/.gnupg'
-        # clean up keys if exist
-        if os.path.exists(gnupg_path):
+        self.gpg = gnupg.GPG(gnupghome=gnupg_path)
+        self.gpg.encoding = 'utf-8'
+        # import public key if exist
+        if os.path.exists(self.exported_public_key_file):
             print 'exist'
-            shutil.rmtree(gnupg_path)
+            with open(self.exported_public_key_file, 'rb') as key_file:
+                key_data = key_file.read()
+                import_result = self.gpg.import_keys(key_data)
+                self.fp = import_result.fingerprints
+                print('Imported public key fingerprint:[%s]' % self.fp)
+        # run at the first time, generate and export key pair
         else:
-            print 'not exist'
-        self.exported_key_file = gnupg_path+'/exported_key.private'
-        self.gnupg_path = gnupg_path[:-7]
-        gpg = gnupg.GPG(gnupghome=gnupg_path)
-        gpg.encoding = 'utf-8'
-        key = gpg.gen_key(gpg.gen_key_input(key_type="RSA", key_length=1024))
-        self.gpg = gpg
-        self.fp = key.fingerprint
-        ascii_armored_private_keys=gpg.export_keys(self.fp, True)
-        with open(self.exported_key_file, 'w') as the_key:
-            the_key.write(ascii_armored_private_keys)
-        print 'Export pk:[%s]' % self.exported_key_file
+            print 'the first time'
+            key = self.gpg.gen_key(self.gpg.gen_key_input(key_type="RSA", key_length=1024))
+            self.fp = key.fingerprint
+            ascii_armored_public_keys = self.gpg.export_keys(self.fp)
+            ascii_armored_private_keys = self.gpg.export_keys(self.fp, True)
+            with open(self.exported_public_key_file, 'w') as the_public_key:
+                the_public_key.write(ascii_armored_public_keys)
+            print('Exported public key:[%s]' % self.exported_public_key_file)
+            with open(self.exported_private_key_file, 'w') as the_key:
+                the_key.write(ascii_armored_private_keys)
+            print('Exported private key:[%s]' % self.exported_private_key_file)
 
+    #store both encrypted and locally decrypted file
     def do_encrypt(self, file_path):
-        self.encoded_file = file_path+'.encoded'
-        savefile = file_path+'.decoded'
+        self.encrypted_file = file_path+'.encoded'
+        self.try_decoded_file = file_path+'.decoded'
         #encryption
         with open(file_path, 'rb') as content_file:
             data = content_file.read()
-            encrypted_ascii_data = str(self.gpg.encrypt(data, self.fp, output=self.encoded_file))
-            print 'encrypted_data=[%s]' % self.encoded_file
+            encrypted_ascii_data = str(self.gpg.encrypt(data, self.fp, output=self.encrypted_file))
+            print('Encrypted_data=[%s]' % self.encrypted_file)
             #decryption
-            self.gpg.decrypt(encrypted_ascii_data, output=savefile)
-            print 'decrypted_data=[%s]'% savefile
-    
+            self.gpg.decrypt(encrypted_ascii_data, output=self.try_decoded_file)
+            print('Decrypted_data=[%s]' % self.try_decoded_file)
+            return self.encrypted_file
+        return None
+
     def delete_key(self):
-        #delete key
-        print str(self.gpg.delete_keys(self.fp, True))
-        print str(self.gpg.delete_keys(self.fp))
-
-
+        print('Delete GPG private key:[%s]' % str(self.gpg.delete_keys(self.fp, True)))
+        print('Delete GPG public key:[%s]' % str(self.gpg.delete_keys(self.fp)))
 
 
 #    data = raw_input("Enter full path of file to encrypt:")
