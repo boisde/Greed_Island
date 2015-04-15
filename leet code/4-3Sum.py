af3063d90497039ee2c68374a3e8c8d7b2fad992
Chen Xinlu <boisde1117@gmail.com>
2015-04-15 23:08:03 +0800

    3Sum, carefully skip all possible duplicated answer is hard.

--- leet code/4-3Sum.py
+++ leet code/4-3Sum.py
@@ -17,20 +17,45 @@ Note:
 class Solution(object):
     @staticmethod
     # @return a list of lists of length 3, [[val1,val2,val3]]
-    def three_sum_n_cubed(num):
-        # todo: remove duplicated triplet
+    def three_sum_n_square(num):
+        num = sorted(num)
         N = len(num)
         triplets = []
-        for i in range(N):
-            target = 0 - num[i]
-            for j in range(i+1, N):
-                partner = target - num[j]
-                for k in range(j+1, N):
-                    if partner == num[k]:
-                        triplets.append(sorted([num[i], num[j], num[k]]))
+        for i in range(N-2):
+            a = num[i]
+            # if processed, skip. The first element does not have former.
+            if i > 0 and a == num[i-1]:
+                continue
+            bi = i+1
+            ci = N-1
+            # use two pointers to find b,c in elements after a.
+            while bi < ci:
+                ans = a + num[bi] + num[ci]
+                if ans == 0:
+                    triplets.append([a, num[bi], num[ci]])
+                    # if already in answer, skip.
+                    while bi < ci:
+                        bi += 1
+                        ci -= 1
+                        if num[bi]!=num[bi-1] or num[ci]!=num[ci+1]:
+                            break
+                elif ans < 0:
+                    # if already in answer, skip
+                    while bi < ci:
+                        bi += 1
+                        if num[bi]!=num[bi-1]:
+                            break
+                else:
+                    # if already in answer, skip
+                    while bi < ci:
+                        ci -= 1
+                        if num[ci]!=num[ci+1]:
+                            break
         return triplets
 
 
 if __name__ == "__main__":
     S = [-1, 0, 1, 2, -1, -4]
-    print "three_sum(%s)=%s" %(S, Solution.three_sum_n_cubed(S))
+    print "three_sum(%s)=%s" %(S, Solution.three_sum_n_square(S))
+    S = [0,0,0,0]
+    print "three_sum(%s)=%s" %(S, Solution.three_sum_n_square(S))
