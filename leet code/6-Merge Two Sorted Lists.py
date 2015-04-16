5fef83a7e0fc3390f7939ea38d22dab479cfab09
Chen Xinlu <boisde1117@gmail.com>
2015-04-16 17:41:19 +0800

    merge two sorted lists

--- leet code/6-Merge Two Sorted Lists.py
+++ leet code/6-Merge Two Sorted Lists.py
@@ -11,9 +11,11 @@ class ListNode(object):
         self.next = None
 
     def __repr__(self):
-        ll = [self.val]
-        while self.next:
-            ll.append(self.next.val)
+        ll = []
+        cur = self
+        while cur:
+            ll.append(cur.val)
+            cur = cur.next
         return str(ll)
 
 
@@ -22,13 +24,16 @@ class Solution(object):
     # @return a ListNode
     @staticmethod
     def merge_two_lists(m, n):  # linear in sum of length of two lists
+        # TODO improve performance when some list empty
         head, put_away = None, None
         while m or n:
             # find the elem to put away
             if not m:
                 to_put_away = n
+                n = n.next
             elif not n:
                 to_put_away = m
+                m = m.next
             else:
                 if m.val <= n.val:
                     to_put_away = m
@@ -37,21 +42,33 @@ class Solution(object):
                     to_put_away = n
                     n = n.next
             # add link if not the first put_away
-            if not put_away:
+            # always put_away
+            if put_away:
+                put_away.next = to_put_away  # add link
+                put_away = to_put_away
+            # init
+            else:
                 put_away = to_put_away
                 head = put_away
-            else:
-                put_away.next = to_put_away
-                put_away = to_put_away
-                # either list is empty, Done!
-                if not m or not n:
-                    break
         return head
 
+    @staticmethod
+    def merge_two_lists_recursive(m, n):
+        if not m:
+            return n
+        if not n:
+            return m
+        if m.val <= n.val:
+            m.next = Solution.merge_two_lists_recursive(m.next, n)
+            return m
+        else:
+            n.next = Solution.merge_two_lists_recursive(m, n.next)
+            return n
+
 if __name__ == "__main__":
+    m = ListNode(0)
+    m.next = ListNode(4)
+    n = ListNode(1)
+    n.next = ListNode(2)
     print Solution.merge_two_lists(ListNode(0), ListNode(1))
-    # m = ListNode(0)
-    # m.next = ListNode(4)
-    # n = ListNode(1)
-    # n.next = ListNode(2)
-    # print Solution.merge_two_lists(m, n)
+    print Solution.merge_two_lists(m, n)
