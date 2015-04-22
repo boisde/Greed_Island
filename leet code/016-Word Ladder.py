0e8f634a82b36356b6c90ac476776abaf43f08b9
Chen Xinlu <boisde1117@gmail.com>
2015-04-22 21:33:31 +0800

    word ladder: brute force

--- leet code/016-Word Ladder.py
+++ leet code/016-Word Ladder.py
@@ -26,9 +26,30 @@ class Solution(object):
     # @param wordDict, a set<string>
     # @return an integer
     @staticmethod
-    def ladder_length(begin_word, end_word, word_dict):
-        pass
+    def ladder_length(begin_word, end_word, word_dict):  # 暴力：26*要查找的词长度*dict所含有的词数量
+        import collections
+        que = collections.deque([(begin_word, 1)])  # intermediate words and its distance to end_word
+        alpha = [chr(i) for i in range(ord('a'), ord('z')+1)]
+        # 对每个中间词的每个char进行从a到z的替换，
+        # 看下新的词是否在dict里面，是的话，就从dict里面去掉，防止重复计算，如果找到，就返回，不然，就继续从中间词queue中pop来处理。
+        while que:
+            wt = que.popleft()
+            w, w_distance = wt[0], wt[1]
+            for i in range(len(w)):
+                s1 = w[:i]
+                s2 = w[i+1:]
+                for c in alpha:
+                    m = s1 + c + s2
+                    if m == end_word:
+                        return w_distance+1
+                    elif m in word_dict and c != w[i]:
+                        word_dict.remove(m)
+                        que.append((m, w_distance+1))
+        return 0
 
 
 if __name__ == "__main__":
-    pass
+    start = "hit"
+    end = "cog"
+    word_dict = ["hot","dot","dog","lot","log"]
+    print Solution.ladder_length(start, end, word_dict)
