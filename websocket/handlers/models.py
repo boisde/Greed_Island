62c6740454f01641c288862825f12d9c96bf26d9
boisde <boisde1117@gmail.com>
2016-08-08 18:17:01 +0800

    implement receivers for web socket

--- websocket/handlers/models.py
+++ websocket/handlers/models.py
@@ -2,14 +2,67 @@
 # coding:utf-8
 from __future__ import unicode_literals
 
+
+class Dict(dict):
+    """
+    Simple dict but support access as x.y style.
+
+    >>> d1 = Dict()
+    >>> d1['x'] = 100
+    >>> d1.x
+    100
+    >>> d1.y = 200
+    >>> d1['y']
+    200
+    >>> d2 = Dict(a=1, b=2, c='3')
+    >>> d2.c
+    '3'
+    >>> d2['empty']
+    Traceback (most recent call last):
+        ...
+    KeyError: 'empty'
+    >>> d2.empty
+    Traceback (most recent call last):
+        ...
+    AttributeError: 'Dict' object has no attribute 'empty'
+    >>> d3 = Dict(('a', 'b', 'c'), (1, 2, 3))
+    >>> d3.a
+    1
+    >>> d3.b
+    2
+    >>> d3.c
+    3
+    """
+
+    def __init__(self, names=(), values=(), **kw):
+        super(Dict, self).__init__(**kw)
+        for k, v in zip(names, values):
+            self[k] = v
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+
+M_TYPE = Dict({'MAN': 'man', 'STAFF': 'staff'})
+
+# 放db
 _channels = [
     {
+        '_id': '001',
         'name': '系统通知',
+        # 听众
         'man': [
             {
                 'id': '1',
                 'name': '1',
                 'avatar': '1',
+                # 'm_type': M_TYPE.MAN,
                 'unread': 0,
             },
             {
@@ -19,6 +72,7 @@ _channels = [
                 'unread': 0,
             },
         ],
+        # 客服
         'staff': [
             {
                 'id': 'a',
@@ -31,3 +85,11 @@ _channels = [
 ]
 
 channels = {c['name']: 1 for c in _channels}
+
+receivers = {}
+for c in _channels:
+    receivers[c['_id']] = {'man': {}, 'staff': {}}
+    for m in c['man']:
+        receivers[c['_id']]['man'][m['id']] = m
+    for s in c['staff']:
+        receivers[c['_id']]['staff'][s['id']] = s
--- websocket/handlers/websocket.py
+++ websocket/handlers/websocket.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 import tornado.websocket
 
-from models import channels
+from models import channels, receivers
 
 # we gonna store clients in dictionary..
 clients = dict()
@@ -12,24 +12,42 @@ clients = dict()
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
     def __init__(self, application, request, **kwargs):
         self.client_id = None
+        self.m_type = None
         super(WebSocketHandler, self).__init__(application, request, **kwargs)
 
     def open(self, *args):
         print("WebSocket opened")
         self.client_id = self.get_argument("id")
+        self.m_type = self.get_argument("m_type")
         self.stream.set_nodelay(True)
-        clients[self.client_id] = {"id": self.client_id, "object": self}
+        clients[self.client_id] = {"id": self.client_id, "m_type": self.m_type, "object": self}
         print(clients)
 
-    def on_message(self, message):
+    def on_message(self, m_obj):
         """
-        when we receive some message we want some message handler..
-        for this example i will just print message to console
+        找到这个client_id所在的channel里所有应该收到的人的id, 向他们的socket丢一条信息.
+        message := {
+            'channel': '001',
+            'text': 'hello world!'
+        }
         """
-        print("Client %s received a message : %s" % (self.id, message))
-        self.write_message('You[%s] sent me: [%s]' % (self.id, message))
+        # 1. 找到receivers, 记录message, sender, receivers
+        ch_id = m_obj['channel']
+        m_type = self.m_type
+        rs = {}
+        if m_type == 'man':
+            rs = receivers[ch_id]['staff']
+        elif m_type == 'staff':
+            rs = receivers[ch_id]['man']
+
+        # 2. 向receivers丢消息
+        print("Client %s received a message : %s" % (self.client_id, m_obj))
+        for r in rs:
+            r_socket = clients.get(r)
+            if r_socket:
+                self.write_message('Someone[%s] sent me: [%s]' % (r_socket, m_obj['text']))
 
     def on_close(self):
         print("WebSocket closed")
-        if self.id in clients:
-            del clients[self.id]
+        if self.client_id in clients:
+            del clients[self.client_id]
