5236efd58e8442b020e90e43086106aa235bcec5
xinlu <boisde1117@gmail.com>
2020-12-28 23:43:43 +0800

    Update clean.py

--- snippets/clean.py
+++ snippets/clean.py
@@ -16,14 +16,16 @@ TARGET_DIR_NAMES = [
     # "大叔之爱.Ossans.Love",
     # "阿尔罕布拉宫的回忆",
     # "柯明斯基理论.The.Kominsky.Method.S01",
-    "[Lilith-Raws] Shingeki no Kyojin - The Final Season"
+    "[Lilith-Raws] Shingeki no Kyojin - The Final Season",
+    "The.Queens.Gambit.S01"
 ]
 TARGET_FILE_TYPE = ".mkv"
 
 
 def clean_dir(target_dir_name):
+    new_target_dir_name = format_file_name(target_dir_name, is_folder=True)
     # create new target folder
-    new_folder_path = os.path.join(CUR_PATH, target_dir_name)
+    new_folder_path = os.path.join(CUR_PATH, new_target_dir_name)
     if not os.path.exists(new_folder_path):
         os.mkdir(new_folder_path)
         print("[%s] dir created.\n" % new_folder_path)
@@ -44,6 +46,7 @@ def clean_dir(target_dir_name):
                         if f_name.endswith(TARGET_FILE_TYPE):
                             f_path = os.path.join(son_dir_path, f_name)
                             print("Moving [%s]..." % f_path)
+                            f_name = format_file_name(f_name)
                             dst = shutil.move(f_path, os.path.join(new_folder_path, f_name))
                             if dst:
                                 print("Moved to [%s]." % dst)
@@ -55,6 +58,7 @@ def clean_dir(target_dir_name):
                 # mv the file in it
                 filepath = os.path.join(CUR_PATH, filename)
                 print("Moving [%s]..." % filepath)
+                filename = format_file_name(filename)
                 dst = shutil.move(filepath, os.path.join(new_folder_path, filename))
                 if dst:
                     print("Moved to [%s]." % dst)
@@ -66,13 +70,26 @@ def clean_dir(target_dir_name):
     for d_or_f in cleaned:
         if os.path.exists(d_or_f):
             if os.path.isdir(d_or_f):
-                r = shutil.rmtree(d_or_f)
-                if r:
-                    print(r)
+                shutil.rmtree(d_or_f)
             elif os.path.isfile(d_or_f):
-                r = os.remove(d_or_f)
-                if r:
-                    print(r)
+                os.remove(d_or_f)
+
+
+def format_file_name(fn: str, is_folder: bool = False):
+    # print(f"fn=[{fn}].")
+    if ' ' in fn:
+        name_parts = fn.split(' ')
+        new_name_parts = []
+        for part in name_parts:
+            if '[' in part or '-' in part:
+                continue
+            else:
+                new_name_parts.append(part)
+        if not is_folder and TARGET_FILE_TYPE not in new_name_parts[-1]:
+            new_name_parts.append(TARGET_FILE_TYPE[1:])
+        fn = '.'.join(new_name_parts)
+    # print(f"new fn=[{fn}].")
+    return fn
 
 
 if __name__ == "__main__":
