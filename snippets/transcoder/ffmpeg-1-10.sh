18f544e9a65cc055f2d292ac6c8c6456866b7110
xinlu <boisde1117@gmail.com>
2021-03-05 18:15:10 +0800

    add ff-scripts.

---
+++ snippets/transcoder/ffmpeg-1-10.sh
@@ -0,0 +1,10 @@
+ffmpeg -re -i ~/transcoder-testing/test-with-timer.flv -f flv -c:v copy -c:a aac -strict -2 -maxrate 4M rtmp://127.0.0.1/live/dlivetestdlivetestdlivetestdlive_dlivetest-1 &
+ffmpeg -re -i ~/transcoder-testing/test-with-timer.flv -f flv -c:v copy -c:a aac -strict -2 -maxrate 4M rtmp://127.0.0.1/live/dlivetestdlivetestdlivetestdlive_dlivetest-2 &
+ffmpeg -re -i ~/transcoder-testing/test-with-timer.flv -f flv -c:v copy -c:a aac -strict -2 -maxrate 4M rtmp://127.0.0.1/live/dlivetestdlivetestdlivetestdlive_dlivetest-3 &
+ffmpeg -re -i ~/transcoder-testing/test-with-timer.flv -f flv -c:v copy -c:a aac -strict -2 -maxrate 4M rtmp://127.0.0.1/live/dlivetestdlivetestdlivetestdlive_dlivetest-4 &
+ffmpeg -re -i ~/transcoder-testing/test-with-timer.flv -f flv -c:v copy -c:a aac -strict -2 -maxrate 4M rtmp://127.0.0.1/live/dlivetestdlivetestdlivetestdlive_dlivetest-5 &
+ffmpeg -re -i ~/transcoder-testing/test-with-timer.flv -f flv -c:v copy -c:a aac -strict -2 -maxrate 4M rtmp://127.0.0.1/live/dlivetestdlivetestdlivetestdlive_dlivetest-6 &
+ffmpeg -re -i ~/transcoder-testing/test-with-timer.flv -f flv -c:v copy -c:a aac -strict -2 -maxrate 4M rtmp://127.0.0.1/live/dlivetestdlivetestdlivetestdlive_dlivetest-7 &
+ffmpeg -re -i ~/transcoder-testing/test-with-timer.flv -f flv -c:v copy -c:a aac -strict -2 -maxrate 4M rtmp://127.0.0.1/live/dlivetestdlivetestdlivetestdlive_dlivetest-8 &
+ffmpeg -re -i ~/transcoder-testing/test-with-timer.flv -f flv -c:v copy -c:a aac -strict -2 -maxrate 4M rtmp://127.0.0.1/live/dlivetestdlivetestdlivetestdlive_dlivetest-9 &
+ffmpeg -re -i ~/transcoder-testing/test-with-timer.flv -f flv -c:v copy -c:a aac -strict -2 -maxrate 4M rtmp://127.0.0.1/live/dlivetestdlivetestdlivetestdlive_dlivetest-10 &
