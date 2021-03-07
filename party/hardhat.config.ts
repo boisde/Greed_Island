1f51307cfe72e6378c3aeac1e4bb1fa45b825ed6
xinlu <boisde1117@gmail.com>
2021-03-07 23:07:02 +0800

    Update hardhat.config.ts

--- party/hardhat.config.ts
+++ party/hardhat.config.ts
@@ -1,20 +1,15 @@
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 import { HardhatUserConfig } from "hardhat/types";
 import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-etherscan"
 import "hardhat-typechain";
-
-// module.exports = {
-//     solidity: "0.7.3",
-// };
 
 let INFURA_API_KEY = "";
 let RINKEBY_PRIVATE_KEY = "";
+let ETHERSCAN_API_KEY = "";
 const config: HardhatUserConfig = {
     defaultNetwork: "hardhat",
     solidity: {
-        compilers: [{ version: "0.7.3", settings: {} }],
+        compilers: [{ version: "0.8.0", settings: {} }],
     },
     networks: {
         hardhat: {},
@@ -22,6 +17,10 @@ const config: HardhatUserConfig = {
             url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
             accounts: [RINKEBY_PRIVATE_KEY],
         },
+    },
+    etherscan: {
+        // Obtain one at https://etherscan.io/
+        apiKey: ETHERSCAN_API_KEY,
     },
 };
 export default config;
