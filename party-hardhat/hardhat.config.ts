021d193eaccc703230f2063c18201dd9fcb36e7a
xinlu <boisde1117@gmail.com>
2021-03-09 23:30:21 +0800

    use env var for ts.

---
+++ party-hardhat/hardhat.config.ts
@@ -0,0 +1,37 @@
+require("dotenv").config();
+const replace = require("replace-in-file");
+
+import { HardhatUserConfig } from "hardhat/types";
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-etherscan"
+import "hardhat-typechain";
+
+
+const config: HardhatUserConfig = {
+    defaultNetwork: "hardhat",
+    solidity: {
+        compilers: [{ version: "0.7.3", settings: {} }],
+    },
+    networks: {
+        hardhat: {},
+        rinkeby: {
+            url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+            accounts: [process.env.RINKEBY_PRIVATE_KEY],
+        },
+    },
+    etherscan: {
+        // Obtain one at https://etherscan.io/
+        apiKey: process.env.ETHERSCAN_API_KEY,
+    },
+};
+export default config;
+
+// import { HardhatUserConfig } from "hardhat/types";
+// import "@nomiclabs/hardhat-waffle";
+// import "hardhat-typechain";
+// const config: BuidlerConfig = {
+//     solidity: {
+//         compilers: [{ version: "0.6.8", settings: {} }],
+//     },
+// };
+// export default config;
