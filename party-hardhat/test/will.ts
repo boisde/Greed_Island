a2a06c07817f05d859f0ba07588e5a583b6e6725
xinlu <boisde1117@gmail.com>
2021-03-11 13:24:00 +0800

    test profiler.

--- party-hardhat/test/will.ts
+++ party-hardhat/test/will.ts
@@ -3,9 +3,12 @@ import chai from "chai";
 // import { solidity } from "ethereum-waffle";
 import { Will } from "../typechain";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
+
+import { profiler } from "@0x/contracts-test-utils";
 // chai.use(solidity);
 const { expect } = chai;
 
+profiler.start()
 describe('Will', () => {
     let will: Will;
     let owner: SignerWithAddress;
@@ -20,8 +23,11 @@ describe('Will', () => {
     });
 
     describe('Deployment', () => {
+
         it('should set the right owner', async () => {
             expect("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266").to.equal(owner.address);
         });
+
     });
 });
+profiler.stop()
